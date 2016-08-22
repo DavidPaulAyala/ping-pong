1aed40c48a3c26757edaf8bfec27e2d20af237e2
David Ayala <dpayala@hotmail.com>
2016-08-21 20:06:05 -0700

    completes required output exceptions, seperates business and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+var pong = function(input) {
+  var output = " ";
+  for (var i = 1; i <= input; i++) {
+    if (i % 15 === 0){
+      output += "PingPong" + " - ";
+    } else if (i % 5 === 0){
+      output += "Pong" + " - ";
+    } else if (i % 3 === 0) {
+      output += "Ping" + " - ";
+    } else {
+      output += i + " - ";
+    }
+  }
+  return output;
+};
+
+
+
 $(document).ready(function() {
-  $("form#numberEntered").submit(function(event) {
+  $("#input form").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#inputNumber").val());
-    $("#output").text(number1);
+    var countUpTo = parseInt($("input#number").val());
+    output = [pong(countUpTo)]
+    $("#result").text(output);
   });
 });
